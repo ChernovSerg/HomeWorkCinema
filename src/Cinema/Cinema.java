67ee0a732b4e6d6b835977a94674b01d532af6fc
Sergey Chernov <chernov_serg@mail.ru>
2019-01-19 19:07:43 +0300

    branch1

--- src/Cinema/Cinema.java
+++ src/Cinema/Cinema.java
@@ -79,10 +79,7 @@ public class Cinema {
         cinema.removeSeance(seance4,Days.FRIDAY);
         cinema.removeMovie(movie1);
         System.out.println("branch1");
-        System.out.println("Вносим изменения в ветку master");
-        System.out.println("Вносим еще изменение в ветку brach1");
-        System.out.println("Вносим еще изменение в ветку brach1");
-        System.out.println("Вносим еще изменение в ветку brach1");
+        System.out.println("branch1");
     }
 
 }
